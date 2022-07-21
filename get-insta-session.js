b6ab9698b7ad670652e8525486092813ad8acc6e
Mahesh Kumar <81576386+jacktheboss220@users.noreply.github.com>
2022-07-21 09:03:31 +0530

    Update get-insta-session.js

--- get-insta-session.js
+++ get-insta-session.js
@@ -2,6 +2,6 @@ let { igApi, getSessionId } = require('insta-fetcher');
 let username='Your user name here';
 let pass='your password here';
 (async () => {
-    const session_id = await getSessionId('username', 'password');
-    console.log('Copy this :- ',session_id);
+  const session_id = await getCookie(username, password);
+  console.log(session_id);
 })();
